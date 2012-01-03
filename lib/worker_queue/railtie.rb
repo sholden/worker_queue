e26009a07e9a98a503ae81423ac895ca5a7e019d
Scott Holden <scott@sshconnection.com>
2012-01-03 10:25:11 -0800

    moved tasks

--- lib/worker_queue/railtie.rb
+++ lib/worker_queue/railtie.rb
@@ -2,10 +2,8 @@ require 'worker_queue'
 
 module WorkerQueue
   class Railtie < ::Rails::Railtie
-    railtie_name :worker_queue
-    
     rake_tasks do
-      require 'tasks/worker_queue.tasks'
+      load "worker_queue/tasks/worker_queue.rake"
     end
   end
 end
